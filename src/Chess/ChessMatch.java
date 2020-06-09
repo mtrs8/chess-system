a4ac2e7907c23617bbc856b13b0bb4a4d604c467
Matheus Rodrigues <mr634580@gmail.com>
2020-06-09 16:34:31 -0300

    Check logic - bug

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -2,6 +2,7 @@ package Chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import Boardgame.Board;
 import Boardgame.Piece;
@@ -14,11 +15,13 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	private boolean check;
 	
 	private List<Piece> piecesOnTheBoard;
 	private List<Piece> capturedPieces;
 	
 	public ChessMatch(){
+		this.check = false;
 		this.board = new Board(8, 8);
 		this.turn = 1;
 		this.currentPlayer = Color.WHITE;
@@ -34,6 +37,10 @@ public class ChessMatch {
 	
 	public Color getCurrentPlayer(){
 		return this.currentPlayer;
+	}
+	
+	public boolean getCheck(){
+		return this.check;
 	}
 	
 	// Cria uma partida de Xadrez
@@ -61,6 +68,12 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		
+		if(testCheck(currentPlayer)){
+			undoMove(source, target, capturedPiece);
+			throw new ChessException("You can't put yourself in check!");
+		}
+		check = (testCheck(opponent(currentPlayer))) ? true : false;
 		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
@@ -76,6 +89,17 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 		return capturedPiece;
+	}
+	
+	private void undoMove(Position source, Position target, Piece capturedPiece){
+		Piece p = board.removePiece(target);
+		board.placePiece(p, source);
+		
+		if(capturedPiece != null){
+			board.placePiece(capturedPiece, target);
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+		}
 	}
 	
 	// Sub-Rotina para movimentação das peças
@@ -95,9 +119,43 @@ public class ChessMatch {
 			throw new ChessException("The chosen piece can't move to target position!");
 	}
 	
+	//Cria turnos entre os jogadores branco e preto
 	private void nextTurn(){
 		this.turn++;
 		this.currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	}
+	
+	private Color opponent(Color color){
+		return (color == Color.WHITE)? Color.BLACK : Color.WHITE;
+	}
+	
+	private ChessPiece king(Color color){
+		List<Piece> list = piecesOnTheBoard
+				.stream().filter(x -> ((ChessPiece)x)
+				.getColor()== color).collect(Collectors.toList());
+		
+		for(Piece p : list) {
+			if(p instanceof King){
+				return (ChessPiece) p;
+			}
+		}
+		throw new IllegalStateException("There is no " + color + " king on the board!");
+	}
+	
+	//Verifica se o Rei está em cheque
+	private boolean testCheck(Color color){
+		Position kingPosition = king(color).getChessPosition().toPosition();
+		List<Piece> opponentPieces = piecesOnTheBoard
+				.stream().filter(x -> ((ChessPiece)x)
+				.getColor() == opponent(color))
+				.collect(Collectors.toList());
+		
+		for(Piece p : opponentPieces){
+			boolean[][] mat = p.possibleMoves();
+			if(mat[kingPosition.getRow()][kingPosition.getColumn()])
+				return true;
+		}
+		return false;
 	}
 	
 	// Faz uma "tradução" entre o modelo padrão e o modelo de matrizes
